faa1e3e33cc34529e889ee0c44e2d9e32cfab3ad
Vinay Patel <vinay.light@hotmail.co.uk>
2020-10-27 11:58:06 +0000

    update

--- GymTracker_DeploymentVersion/GymTrackerA2WPFLayer/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ GymTracker_DeploymentVersion/GymTrackerA2WPFLayer/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -237,7 +237,7 @@ namespace GymTrackerGui {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GymTrackerGUI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GymTrackerGUI;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
